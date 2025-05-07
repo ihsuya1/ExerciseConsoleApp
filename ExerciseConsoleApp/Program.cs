feb143eb8efb26d70c236cbf7542c86cc94938e9
Ayushi Goswami <goswamay@sheridancollege.ca>
2025-05-07 14:37:21 -0400

    Add project files.

---
+++ ExerciseConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ExerciseConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 0, 30 };
+            int total = 0;
+            int count = numbers.Length;
+
+            for (int i = 0; i <= count; i++) // off-by-one error
+            {
+                total += numbers[i];
+            }
+
+            int average = total / count;
+            Console.WriteLine("Average: " + average);
+        }
+    }
+}
